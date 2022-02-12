86f8727bf8e991bce5f35e4b9bfc26eafcf64f46
maanHimself <maanasdf87@gmail.com>
2022-02-12 15:59:34 +0530

    f

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,52 @@ import FilterDesktop from "../components/FilterDesktop";
 
 import { useState, useEffect } from "react";
 
-function Phys(props) {
-  const breakpoint = 769;
-  const [width, setWidth] = useState(0);
+import { debounce } from "lodash";
+
+function useWindowSize() {
+  const [dimensions, setDimensions] = useState({
+    height: 0,
+    width: 0,
+  });
 
   useEffect(() => {
-    width = setWidth(Window.innerWidth);
-    window.addEventListener("resize", () => setWidth(window.innerWidth));
-    return () => window.removeEventListener("resize", handleWindowResize);
+    setDimensions({
+      height: window.innerHeight,
+      width: window.innerWidth,
+    });
+
+    //debounce will make the function run only once when the resize is done
+    const handleWindowResizeThrottle = debounce(
+      function handleResize() {
+        setDimensions({
+          height: window.innerHeight,
+          width: window.innerWidth,
+        });
+      },
+      400,
+      { leading: false, trailing: true }
+    );
+
+    window.addEventListener("resize", handleWindowResizeThrottle, {
+      passive: true,
+    });
+    return () => {
+      window.removeEventListener("resize", handleWindowResizeThrottle);
+    };
   }, []);
+
+  return dimensions;
+}
+
+function Phys(props) {
+  const breakpoint = 769;
+  const { width, height } = useWindowSize();
+
+  // useEffect(() => {
+  //   width = setWidth(Window.innerWidth);
+  //   window.addEventListener("resize", () => setWidth(window.innerWidth));
+  //   return () => window.removeEventListener("resize", handleWindowResize);
+  // }, []);
 
   return width < breakpoint ? <FilterMobile /> : <FilterDesktop />;
 }
